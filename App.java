7f56d11aa0dc55147b5a4c5189daf753dbf079ad
koyaharikrishna <koyaharikrishna@koyas-MacBook-Air.local>
2026-04-09 21:46:28 +0530

    first commit

---
+++ App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Java App Started...");
+
+        while (true) {
+            System.out.println("Running Java App...");
+            Thread.sleep(5000);
+        }
+    }
+}
